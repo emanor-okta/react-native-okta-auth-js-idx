bdfb648223bff7f0c2993088b9545b57b6978883
Erik Manor <erik.manor@okta.com>
2024-02-05 23:17:37 -0800

    config

--- okta/Config.js
+++ okta/Config.js
@@ -1,10 +1,7 @@
 export default config = {
-    issuer: 'https://gw.oktamanor.net/oauth2/default',
-    clientId: '0oa3nwni9dcqDqW0z1d7',
-    redirectUri: 'com.erikdevelopernot.oie://callback',
 
-    //issuer: 'https://{DOMAIN}/oauth2/default',
-    //clientId: '0oa3nwni....',
-    //redirectUri: 'com.{DOMAIN}.oie://callback',
+    issuer: 'https://{DOMAIN}/oauth2/default',
+    clientId: '0oa3nwni....',
+    redirectUri: 'com.{DOMAIN}.oie://callback',
     scopes: ['openid', 'profile', 'email', 'offline_access'],  
   }
